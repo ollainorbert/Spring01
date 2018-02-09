33b410f959c1a4ca1807a68a39cc9ae81313b5b6
Norbert Ollai <norbert_ollai@epam.com>
2018-02-09 11:42:24 +0100

    v2.6.1 logger v2

--- src/main/java/com/example/demo/controllers/DemoJSPController.java
+++ src/main/java/com/example/demo/controllers/DemoJSPController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controllers;
 
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DemoJSPController {
 	private final static String ROUTING_ROOT = "/";
 	private final static String ROUTING_INFORMATION = "/information";
+	
+	//private static final Logger logger = LogManager.getLogger(DemoJSPController.class);
+	private static final Logger logger = LoggerFactory.getLogger(DemoJSPController.class);
 	
 	public static String GetRoutingRoot() {
 		return ROUTING_ROOT;
@@ -18,11 +25,21 @@ public class DemoJSPController {
 	
 	@RequestMapping(ROUTING_ROOT)
 	public String index() {
+		logger.error("ROUTE: Root");
+		System.out.println(logger.isDebugEnabled()); // mindig false
+		System.out.println(logger.isTraceEnabled()); // mindig false
+		
+		System.out.println(logger.isErrorEnabled()); // mindig true
+		//System.out.println(logger.isFatalEnabled()); log4j-nel van, slf4j-nel nincs // mindig true
+		System.out.println(logger.isInfoEnabled());	// mindig true
+		System.out.println(logger.isWarnEnabled()); // mindig true
+		
 		return "index";
 	}
 	
 	@RequestMapping(ROUTING_INFORMATION)
 	public String information() {
+		logger.info("ROUTE: Information");
 		return "information";
 	}
 }
