6acb5bd9181f92f3618270250136879ecac81bdf
Norbert Ollai <norbert_ollai@epam.com>
2018-02-09 14:47:03 +0100

    v2.6.4 some more fix

--- src/main/java/com/example/demo/interceptors/DemoInterceptor.java
+++ src/main/java/com/example/demo/interceptors/DemoInterceptor.java
@@ -9,11 +9,12 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class DemoInterceptor extends HandlerInterceptorAdapter {
 	private static final Logger logger = LoggerFactory.getLogger(DemoInterceptor.class);
+	private static final String START_TIME_ID = "startTime";
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		request.setAttribute("startTime", System.currentTimeMillis());
+		request.setAttribute(START_TIME_ID, System.currentTimeMillis());
 		// if returned false, we need to make sure 'response' is sent
 		return true;
 	}
@@ -22,14 +23,14 @@ public class DemoInterceptor extends HandlerInterceptorAdapter {
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
 
-		long endTime = System.currentTimeMillis();
-		long startTime = (Long) request.getAttribute("startTime");
-		long elapsedTime = endTime - startTime;
-
-		if(logger.isInfoEnabled()) {
+		if (logger.isInfoEnabled()) {
+			long endTime = System.currentTimeMillis();
+			long startTime = (Long) request.getAttribute(START_TIME_ID);
+			long elapsedTime = endTime - startTime;
+			
 			logger.info("Request method: " + request.getMethod());
 			logger.info("Request URL: " + request.getRequestURL().toString());
 			logger.info(String.format("%s: %d %s", "Elapsed Time between handle and exit", elapsedTime, "ms"));
-		}	
+		}
 	}
 }
